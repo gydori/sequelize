58f3b5f4e9a9c0c878bbdc76bdcc8cf5123f3729
Gyurcsik Dóra <gyurcsik.dora@gmail.com>
2019-07-08 20:43:07 +0200

    insert row draw table

--- insert.js
+++ insert.js
@@ -1,8 +1,7 @@
-// insert into games (name, description) values("", "");
-
-let readline = require('readline-sync');
-let name = readline.question('Give the name of the game!\n');
-let desc = readline.question('Give a description of the game!\n');
+let readlineSync = require('readline-sync');
+const table = require('table');
+let name = readlineSync.question('Give the name of the game!\n');
+let desc = readlineSync.question('Give a description of the game!\n');
 
 let str = 'insert into games (name, description) values ("' + name + '", "' + desc + '");';
 console.log(str);
@@ -14,7 +13,23 @@ sequelize
   .authenticate()
   .then(() => {
     sequelize.query(str).then(result => {
-      console.log('beszúrtam');
+    });
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+sequelize
+  .authenticate()
+  .then(() => {
+    sequelize.query('select * from games').then(result => {
+      let rows = result[0];
+      let matrix = [];
+      for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        matrix.push([row.id, row.name, row.description]);
+      }
+      console.log(table.table(matrix));
     });
   })
   .catch(err => {
